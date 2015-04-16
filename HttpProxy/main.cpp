b8abd92e72a54779e6dbe239fb87824ffdfd90e4
TracyQiu <qcs4tracy@gmail.com>
2015-04-16 13:59:04 -0500

    Initial Commit

---
+++ HttpProxy/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HttpProxy
+//
+//  Created by QiuChusheng on 15/4/16.
+//  Copyright (c) 2015年 QiuChusheng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
